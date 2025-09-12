cd88ada08848c122df81a33a86443b311501cb40
Philippe-62000 <phimjc@gmail.com>
2025-09-12 23:03:19 +0200

    Fix route test-email-config - Ajout endpoint manquant

--- backend/routes/sickLeaves.js
+++ backend/routes/sickLeaves.js
@@ -14,6 +14,7 @@ router.get('/test-upload', sickLeaveController.testUpload);
 
 // Test de configuration email
 router.get('/test-email', sickLeaveController.testEmailConfiguration);
+router.get('/test-email-config', sickLeaveController.testEmailConfiguration);
 
 // Route publique pour l'upload d'arrêt maladie (salariés)
 router.post('/upload', 
