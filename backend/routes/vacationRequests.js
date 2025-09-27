c11435fbec44583a49b2af10d199771a1db70b39
Philippe-62000 <phimjc@gmail.com>
2025-09-27 22:06:38 +0200

    Fix: Ajouter routes manquantes PATCH /vacation-requests/:id et /accept pour modification et acceptation des demandes de congés

--- backend/routes/vacationRequests.js
+++ backend/routes/vacationRequests.js
@@ -7,7 +7,9 @@ router.get('/', vacationRequestController.getAllVacationRequests);
 router.get('/:id', vacationRequestController.getVacationRequestById);
 router.post('/', vacationRequestController.createVacationRequest);
 router.put('/:id', vacationRequestController.updateVacationRequest);
+router.patch('/:id', vacationRequestController.updateVacationRequest);
 router.patch('/:id/validate', vacationRequestController.validateVacationRequest);
+router.patch('/:id/accept', vacationRequestController.validateVacationRequest);
 router.patch('/:id/reject', vacationRequestController.rejectVacationRequest);
 
 module.exports = router;
